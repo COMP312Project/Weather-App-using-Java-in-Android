597fd32cc64a107f26ead04f131039667385b1ce
COMP312Project <COMP312project@gmail.com>
2025-03-21 12:59:30 -0500

    Implemented dependencies

---
+++ app/src/main/java/com/frsarker/weatherapp/WeatherApiService.java
@@ -0,0 +1,12 @@
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherApiService {
+    @GET("weather")
+    Call<WeatherResponse> getCurrentWeather (
+            @Query("q") String cityName,
+            @Query("appid") String apiKey,
+            @Query("units") String units // e.g., "metric" or "imperial"
+    );
+}
