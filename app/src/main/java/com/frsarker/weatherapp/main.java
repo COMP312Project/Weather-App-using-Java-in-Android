a5802941cf201fa3b121702264cee0fead39f582
COMP312Project <COMP312project@gmail.com>
2025-04-12 00:40:40 -0500

    Added a main.java

---
+++ app/src/main/java/com/frsarker/weatherapp/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class WeatherCLI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Weather App!");
+        System.out.println("Type 'exit' to quit.");
+        System.out.println("Please enter your weather details: ");
+
+        if (input.equalsIgnoreCase("exit")) {
+            System.out.println("Goodbye!");
+            break;
+        }
+    }
+}
